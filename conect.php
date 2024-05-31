fd87719fd1bbc9fe61168fe5e7a4a140c620f2d8
CaioRD <caiords67@gmail.com>
2024-05-31 17:41:29 -0300

    add mudance

--- conect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require __DIR__. '/vendor/autoload.php';
-use Kreait\Firebase\Factory;
-$factory = (new Factory()) ->  withDatabaseUri('https://aula-408dc-default-rtdb.firebaseio.com/');
-$database = $factory -> createDatabase();
-
-?>
